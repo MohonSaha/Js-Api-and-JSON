df5497d81684e210515161be42fcab6224ae87fc
MohonSaha <practicewithmohon@gmail.com>
2023-02-23 11:47:22 +0600

    first commit

---
+++ json.js
@@ -0,0 +1,14 @@
+const user = {id: 1, name: "Mohon Saha", job: "Web Developer"};
+const stringfied = JSON.stringify(user);
+
+// console.log(user);
+// console.log(stringfied);
+
+// Js:
+// { id: 1, name: 'Mohon Saha', job: 'Web Developer' }
+
+// Json: It will double quoted everything excepts number and boolean.
+// {"id":1,"name":"Mohon Saha","job":"Web Developer"}
+
+
+// More complex object:
